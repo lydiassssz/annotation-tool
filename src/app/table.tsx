2f85adc77e7c93f63101bacd3448628c7cbe5e15
lydia <mmy.tanpopo@gmnail.com>
2024-08-05 17:27:52 +0900

    アノテーションツールに必要な最低限までの実装(機能3まで)

---
+++ src/app/table.tsx
@@ -0,0 +1,84 @@
+const Table = ({
+  testTable,
+  cursor,
+  setCursor,
+  handleLabelChange,
+  predictedLabel,
+  calculatePredictedLabel,
+}) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Number</th>
+          <th>Text</th>
+          <th>Speaker Code</th>
+          <th>From</th>
+          <th>To</th>
+          <th>Label</th>
+          <th>New Label</th>
+        </tr>
+      </thead>
+      <tbody>
+        {testTable.map((row, index) => {
+          const isInvalid = !isNaN(row.new_label) && row.new_label >= row.number
+          const isEmpty = isNaN(row.new_label)
+
+          return (
+            <tr
+              key={row.number}
+              style={{
+                backgroundColor: cursor === index ? '#f0f0f0' : 'transparent',
+              }}
+            >
+              <td>{row.number}</td>
+              <td>{row.text}</td>
+              <td>{row.speaker_code}</td>
+              <td>{row.from}</td>
+              <td>{row.to}</td>
+              <td>{row.label}</td>
+              <td
+                style={{
+                  position: 'relative', // Set position to relative to allow absolute positioning within it
+                }}
+              >
+                <input
+                  type="text"
+                  value={isNaN(row.new_label) ? '' : row.new_label}
+                  onChange={(e) => handleLabelChange(index, e.target.value)}
+                  onClick={() => setCursor(index)}
+                  style={{
+                    backgroundColor: isEmpty
+                      ? 'transparent'
+                      : isInvalid
+                        ? '#fdd'
+                        : 'transparent',
+                    color: isInvalid ? '#a00' : 'inherit',
+                    position: 'relative',
+                  }}
+                />
+                {cursor === index && isEmpty && (
+                  <span
+                    style={{
+                      position: 'absolute',
+                      left: '5px',
+                      top: '50%',
+                      transform: 'translateY(-50%)',
+                      color: predictedLabel !== row.label ? '#ff9999' : 'gray',
+                      pointerEvents: 'none',
+                      zIndex: 0,
+                    }}
+                  >
+                    {predictedLabel}
+                  </span>
+                )}
+              </td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
+
+export default Table
